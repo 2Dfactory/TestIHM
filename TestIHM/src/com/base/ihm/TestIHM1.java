ad224def4d99e7033e8ceda37ad78b54e54c2b24
gittesteur <TestGame@orange.fr>
2018-02-15 21:12:58 +0100

    Création du projet

---
+++ TestIHM/src/com/base/ihm/TestIHM1.java
@@ -0,0 +1,14 @@
+package com.base.ihm;
+
+import javax.swing.JFrame;
+
+public class TestIHM1 {
+
+	public static void main(String[] args) {
+
+		JFrame fenetre = new JFrame();
+		fenetre.setVisible(true);
+		
+	}
+
+}
