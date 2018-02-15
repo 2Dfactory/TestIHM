d02f1523cb3e122ed16b36612c2a7b0125fcecc5
gittesteur <TestGame@orange.fr>
2018-02-15 21:28:49 +0100

    Paramétrage de la fenêtre

--- TestIHM/src/com/base/ihm/TestIHM1.java
+++ TestIHM/src/com/base/ihm/TestIHM1.java
@@ -6,7 +6,22 @@ public class TestIHM1 {
 
 	public static void main(String[] args) {
 
+		//Instanciation d'une fenêtre
 		JFrame fenetre = new JFrame();
+		
+		//Ajout d'un titre à la fenêtre
+		fenetre.setTitle("Ma première fenêtre JAVA");
+		
+		//Définition de ses dimensions (400 pixels de large x 100 pixels de haut
+		fenetre.setSize(400, 100);
+		
+		//Positionnement de la fenêtre au centre de l'écran
+		fenetre.setLocationRelativeTo(null);
+		
+		//Termine le processus quand on click sur la croix rouge
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Affiche la fenêtre à l'écran
 		fenetre.setVisible(true);
 		
 	}
