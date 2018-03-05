b59ce604eea958783c7ba21dea2848c2408b923e
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-03-05 13:28:41 +0100

    Màj => Effacement du panel avant chaque affichage du rond rouge 	=> Ajout de commentaires

--- TestIHM/src/com/ihm/panels/PanneauRondMove.java
+++ TestIHM/src/com/ihm/panels/PanneauRondMove.java
@@ -7,11 +7,17 @@ import javax.swing.JPanel;
 
 public class PanneauRondMove extends JPanel{
 
+	//Variable servant à définir les coordonnées du rond rouge
 	private int posX = -50;
 	private int posY = -50;
 
 	public void paintComponent(Graphics g) {
 		
+		//Création d'un rectangle de la surface du panel afin "d'effacer" l'affichage précédant
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
+		//Création d'un rond rouge
 		g.setColor(Color.RED);
 		g.fillOval(posX, posY, 50, 50);
 		
